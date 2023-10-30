5ad4e1053393022825f8f0439e1a172161badc5a
Artiom Porcescu <43860271+Artiom-Porcescu@users.noreply.github.com>
2023-10-30 09:01:37 +0200

    second task implemented

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import csv
+from collections import defaultdict
+from datetime import datetime, timedelta
 
-# Чтение исходного CSV-файла
+#first task
 with open('dataset.csv', 'r') as file:
     reader = csv.reader(file)
     data = list(reader)
 
-# Разделение на даты и данные
 dates = [row[0] for row in data]
 values = [row[1] for row in data]
 
-# Запись в файлы X.csv и Y.csv
 with open('X.csv', 'w', newline='') as x_file, open('Y.csv', 'w', newline='') as y_file:
     x_writer = csv.writer(x_file)
     y_writer = csv.writer(y_file)
@@ -17,3 +17,19 @@ with open('X.csv', 'w', newline='') as x_file, open('Y.csv', 'w', newline='') as
     for date, value in zip(dates, values):
         x_writer.writerow([date])
         y_writer.writerow([value])
+
+#second task
+with open('dataset.csv', 'r') as file:
+    reader = csv.reader(file)
+    data = list(reader)
+
+data_by_year = defaultdict(list)
+for row in data:
+    year = row[0][:4]
+    data_by_year[year].append(row)
+
+for year, year_data in data_by_year.items():
+    output_file_name = f"{year}0101_{year}1231.csv"
+    with open(output_file_name, 'w', newline='') as output_file:
+        writer = csv.writer(output_file)
+        writer.writerows(year_data)
